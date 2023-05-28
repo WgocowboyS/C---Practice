0162d40f76685d3dde68ae063f756f0001364370
WgocowboyS <wgocowboys@gmail.com>
2023-05-28 17:53:36 -0400

    more temp in main

--- Library Management/main.cpp
+++ Library Management/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <vector>
 #include "bookClass.h"
+#include "patronClass.h"
 
 using namespace std;
 
 int main()
 {
-    book temp("Book A", "John H", "Fiction", false, 1234);
+    vector <book> empty;
+    book bookTemp("Book A", "John H", "Fiction", false, 1234);
+    empty.push_back(bookTemp);
+    patron temp("John H",1234, empty);
 
-
+    cout << temp.getId() << endl;
 
     return 0;
 }
