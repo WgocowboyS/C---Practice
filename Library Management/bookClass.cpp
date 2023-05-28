2ececff05e813381534cdd898aff3f66d56383ee
WgocowboyS <wgocowboys@gmail.com>
2023-05-28 16:03:55 -0400

    addded .h and .cpp files for book class

---
+++ Library Management/bookClass.cpp
@@ -0,0 +1,46 @@
+#include "bookClass.h"
+
+using namespace std;
+
+// constructor
+book::book(string title, string author, string genre, bool checkedOut, int id)
+    : title(title),
+      author(author),
+      genre(genre),
+      checkedOut(checkedOut),
+      id(id)
+{
+    // body of constructor (optional)
+}
+
+// getter functions
+string book::getTitle()
+{
+    return title;
+}
+
+string book::getAuthor()
+{
+    return author;
+}
+
+string book::getGenre()
+{
+    return genre;
+}
+
+bool book::getStatus()
+{
+    return checkedOut;
+}
+
+int book::getId()
+{
+    return id;
+}
+
+// setter functions
+void book::setStatus(bool Status)
+{
+    checkedOut = Status;
+}
---
+++ Library Management/bookClass.h
@@ -0,0 +1,40 @@
+
+#ifndef BOOKCLASS_H
+#define BOOKCLASS_H
+
+#include <string>
+
+/*
+ title, author, genre, status (checked in/checked out), and id. 
+ Provide public methods for accessing and 
+ modifying these fields (getters and setters).
+*/
+using namespace std;
+
+class book
+{
+    private:
+    string title;
+    string author;
+    string genre;
+    bool checkedOut;
+    int id;
+
+    public:
+    //constructor
+    book (string title, string author, string genre, bool checkedOut, int id);
+
+    //getter functions
+    string getTitle();
+    string getAuthor();
+    string getGenre();
+    bool getStatus();
+    int getId();
+
+    //set functions
+    void setStatus(bool Status);
+
+
+};
+
+#endif BOOKCLASS_H
