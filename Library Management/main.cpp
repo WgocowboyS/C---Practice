8d8bd60eb9dabfa579088407738b69c190960f42
WgocowboyS <wgocowboys@gmail.com>
2023-05-28 18:44:33 -0400

    added create patron and book

--- Library Management/main.cpp
+++ Library Management/main.cpp
@@ -2,17 +2,83 @@
 #include <vector>
 #include "bookClass.h"
 #include "patronClass.h"
+#include "libraryClass.h"
 
 using namespace std;
 
+// passes lib by reference
+void createPatron(library &lib)
+{
+    string name;
+    int id = 0;
+
+    cout << "Enter a name for the new patron: ";
+    cin.ignore();
+    getline(cin, name);
+    cout << "Enter an unique ID for the patron: ";
+    cin >> id;
+    vector<book> empty;
+
+    patron newPatron(name, id, empty);
+    lib.addPatron(newPatron);
+}
+
+void createBook(library &lib)
+{
+    string name;
+    string author;
+    string genre;
+    bool checkedOut = false; // intially book is never checked out
+    int id;
+
+    cout << "Enter a name for the new book: ";
+    cin.ignore();
+    getline(cin, name);
+    cout << "Enter a author for the new book: ";
+    //cin.ignore();
+    getline(cin, author);
+    cout << "Enter a genre for the new book: ";
+    //cin.ignore();
+    getline(cin, genre);
+    cout << "Enter an unique ID for the book: ";
+    cin >> id;
+
+    book newBook(name, author, genre, checkedOut, id);
+    lib.addBook(newBook);
+}
+
+int userChoice()
+{
+    int choice = -1;
+    while (choice < 0 || choice > 4)
+    {
+        cout << "Enter 1 (add new patron), 2 (add new book), 3 (print all books), 4 (print all patrons), 0 (to end)";
+        cin >> choice;
+    }
+    return choice;
+}
+
 int main()
 {
-    vector <book> empty;
-    book bookTemp("Book A", "John H", "Fiction", false, 1234);
-    empty.push_back(bookTemp);
-    patron temp("John H",1234, empty);
+    // declare library
+    // create two patrons
+    // create three books
 
-    cout << temp.getId() << endl;
+    library centerLibrary;
+    int choice = userChoice();
 
+    while (choice != 0)
+    {
+        if (choice == 1)
+            createPatron(centerLibrary);
+        else if (choice == 2)
+            createBook(centerLibrary);
+        else if (choice == 3)
+            centerLibrary.printAllBooks();
+        else if (choice == 4)
+            centerLibrary.printAllPatrons();
+    choice = userChoice();
+    }
+    cout << "Ending Program\n" << endl;
     return 0;
 }
